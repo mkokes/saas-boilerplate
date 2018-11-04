5399c1ecc0cebe5c617c173678c5f083ed4937d8
Alfonso M. García Astorga <me@alfon.io>
2018-11-04 22:48:56 +0100

    update layout file

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -1,31 +1,12 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// import Navbar from '../components/Navbar';
-// import Footer from '../components/Footer';
+const DefaultLayout = ({ children }) => (
+  <React.Fragment>{children}</React.Fragment>
+);
 
-// const DefaultLayout = ({ children }) => (
-//   <React.Fragment>
-//     <Navbar />
-//     {children}
-//     <Footer />
-//   </React.Fragment>
-// );
+DefaultLayout.propTypes = {
+  children: PropTypes.object,
+};
 
-// const UserDashboardLayout = ({ children }) => (
-//   <React.Fragment>
-//     <Navbar navbarBrandLink="/dashboard" />
-//     {children}
-//     <Footer minimal />
-//   </React.Fragment>
-// );
-
-// DefaultLayout.propTypes = {
-//   children: PropTypes.object,
-// };
-// UserDashboardLayout.propTypes = {
-//   children: PropTypes.object,
-// };
-
-// export default DefaultLayout;
-// export { DefaultLayout, UserDashboardLayout };
+export default DefaultLayout;
