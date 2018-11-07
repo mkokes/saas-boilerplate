0f5913c9c522f0fe867f738eddb133ee1d15ea1c
Alfonso M. García Astorga <me@alfon.io>
2018-11-07 12:53:21 +0100

    add homepage demo content

--- client/src/containers/Homepage/index.js
+++ client/src/containers/Homepage/index.js
@@ -4,17 +4,36 @@
  * This is the first thing users see of our App, at the '/' route
  */
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Segment, Container, Header, Button, Divider } from 'semantic-ui-react';
 
-import SafeQuery from 'components/graphql/SafeQuery';
-import { Hello } from 'graphql/queries';
+const Main = styled.main`
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+`;
+
+const IntroSegment = styled(Segment)`
+  &&& {
+    padding-top: 7em;
+    padding-bottom: 7em;
+    margin-bottom: 0px;
+  }
+`;
+const ContentSegment = styled(Segment)`
+  display: flex;
+  flex-grow: 1;
+  align-items: center;
+`;
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
     return (
-      <main>
+      <Fragment>
         <Helmet>
           <title>Home Page</title>
           <meta
@@ -22,18 +41,46 @@ export default class HomePage extends React.PureComponent {
             content="A React.js Boilerplate application homepage"
           />
         </Helmet>
-        <SafeQuery query={Hello}>
-          {({ data }) =>
-            data ? (
-              <div>
-                <h1>{JSON.stringify(data)}</h1>
-              </div>
-            ) : (
-              <div>No parties to show!</div>
-            )
-          }
-        </SafeQuery>
-      </main>
+        <Main>
+          <IntroSegment textAlign="center" inverted>
+            <Container text>
+              <Header as="h1" inverted>
+                Intro title
+              </Header>
+              <Header as="h2" inverted>
+                Make it short and sweet, but not too short so folks dont simply
+                skip over it entirely.
+              </Header>
+              <Link to="/signup">
+                <Button primary size="huge">
+                  Sign up free
+                </Button>
+              </Link>
+            </Container>
+          </IntroSegment>
+          <ContentSegment vertical>
+            <Container text>
+              <Header as="h3">Breaking The Grid, Grabs Your Attention</Header>
+              <p>
+                Instead of focusing on content creation and hard work, we have
+                learned how to master the art of doing nothing by providing
+                massive amounts of whitespace and generic content that can seem
+                massive, monolithic and worth your attention.
+              </p>
+              <Button size="large">Read More</Button>
+              <Divider horizontal>Case Studies</Divider>
+              <Header as="h3">Did We Tell You About Our Bananas?</Header>
+              <p>
+                Yes I know you probably disregarded the earlier boasts as
+                non-sequitur filler content, but its really true. It took years
+                of gene splicing and combinatory DNA research, but our bananas
+                can really dance.
+              </p>
+              <Button size="large">I am interested</Button>
+            </Container>
+          </ContentSegment>
+        </Main>
+      </Fragment>
     );
   }
 }
