dc72920591ce6b7e96768d968665cc27307e27bc
Alfonso M. García Astorga <me@alfon.io>
2018-11-07 12:54:18 +0100

    update default layout

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
+
 const DefaultLayout = ({ children }) => (
-  <React.Fragment>{children}</React.Fragment>
+  <React.Fragment>
+    <Navbar />
+    {children}
+    <Footer />
+  </React.Fragment>
 );
 
 DefaultLayout.propTypes = {
