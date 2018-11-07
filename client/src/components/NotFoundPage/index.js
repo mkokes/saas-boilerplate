ac8d19fba1433e3a513a313972f9ced65240a397
Alfonso M. García Astorga <me@alfon.io>
2018-11-07 12:52:41 +0100

    add notfoundpage component

---
+++ client/src/components/NotFoundPage/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * NotFoundPage
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class NotFoundPage extends React.PureComponent {
+  render() {
+    return <div />;
+  }
+}
+
+NotFoundPage.propTypes = {};
+
+export default NotFoundPage;
