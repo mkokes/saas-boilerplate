1ea6eee360373df5209d7d0a5e7a244c482855d9
Alfonso M. García Astorga <me@alfon.io>
2018-11-07 13:29:57 +0100

    add 404 page content

--- client/src/components/NotFoundPage/index.js
+++ client/src/components/NotFoundPage/index.js
@@ -4,14 +4,36 @@
  *
  */
 
-import React from 'react';
-// import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+import { Container, Header, Button } from 'semantic-ui-react';
 
 /* eslint-disable react/prefer-stateless-function */
 class NotFoundPage extends React.PureComponent {
   render() {
-    return <div />;
+    return (
+      <Fragment>
+        <Helmet>
+          <title>404</title>
+          <meta
+            name="description"
+            content="Sorry, the page you are looking for doesn't exist."
+          />
+        </Helmet>
+        <Container as="main" textAlign="center">
+          <Header as="h1" style={{ fontSize: '6em' }}>
+            404
+          </Header>
+          <p style={{ fontSize: '1.5em' }}>
+            Sorry, the page you are looking for doesn&apos;t exist.
+          </p>
+          <Link to="/">
+            <Button size="massive">Return to home page</Button>
+          </Link>
+        </Container>
+      </Fragment>
+    );
   }
 }
 
