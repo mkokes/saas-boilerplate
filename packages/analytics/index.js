545930192c82b0a58859255ccbf2f86cf4860104
Alfonso M. García Astorga <me@alfon.io>
2018-12-06 20:25:57 +0100

    update

--- packages/analytics/index.js
+++ packages/analytics/index.js
@@ -13,7 +13,8 @@ if (PROXY) {
 }
 
 const MIXPANEL_API_URL = 'https://api.mixpanel.com'
-const MIXPANEL_JS_LIB_URL = 'http://cdn4.mxpnl.com/libs/mixpanel-2-latest.min.js'
+const MIXPANEL_JS_LIB_URL =
+  'http://cdn4.mxpnl.com/libs/mixpanel-2-latest.min.js'
 
 const CODE_CACHE = new Map()
 
@@ -25,44 +26,46 @@ const init = async () => {
   const app = new Koa()
   const router = new KoaRouter()
 
-  app.use(cors({
-    credentials: true,
-  }))
+  app.use(
+    cors({
+      credentials: true,
+    }),
+  )
 
   router.get('/client.js', async ctx => {
     const { origin } = ctx
 
     if (!CODE_CACHE[origin]) {
-      CODE_CACHE[origin] = mixpanelJs.replace(MIXPANEL_API_URL, origin.substr(origin.indexOf('://') + 1))
+      CODE_CACHE[origin] = mixpanelJs.replace(
+        MIXPANEL_API_URL,
+        origin.substr(origin.indexOf('://') + 1),
+      )
     }
 
     ctx.body = CODE_CACHE[origin]
   })
-
   router.get('/favicon.ico', async ctx => {
     ctx.body = ''
   })
-
   router.get('*', async ctx => {
     const { origin, href } = ctx
     const newUrl = `${MIXPANEL_API_URL}${href.substr(origin.length)}`
 
     const headers = {
       ...ctx.headers,
-      'X-Forwarded-For': ctx.ip
+      'X-Forwarded-For': ctx.ip,
     }
     delete headers.host
 
     const { body, statusCode, rawHeaders } = await got(newUrl, {
       headers,
-      ...(PROXY ? { agent: false } : null)
+      ...(PROXY ? { agent: false } : null),
     })
 
     ctx.body = body
     ctx.status = statusCode
     ctx.headers = rawHeaders
   })
-
   app.use(router.routes())
   app.use(router.allowedMethods())
 
