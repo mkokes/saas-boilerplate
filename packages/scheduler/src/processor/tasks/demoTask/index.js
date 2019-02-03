32825769914214bef94f250b3eeab37b0983ca63
Alfonso M. García Astorga <me@alfon.io>
2019-02-03 20:52:32 +0100

    add

--- packages/scheduler/src/processor/tasks/demoTask/index.js
+++ packages/scheduler/src/processor/tasks/demoTask/index.js
@@ -1,10 +1,14 @@
 module.exports = ({ log: parentLog }) => {
   const log = parentLog.create('demoTask');
 
-  try {
-    log.info(`Running demoTask ...`);
-    log.info('DONE ✅');
-  } catch (err) {
-    log.error('Failed', err);
-  }
+  return () => {
+    try {
+      log.info(`Running demoTask ...`);
+      setTimeout(() => {
+        log.info('DONE ✅');
+      }, 5000);
+    } catch (err) {
+      log.error('Failed', err);
+    }
+  };
 };
