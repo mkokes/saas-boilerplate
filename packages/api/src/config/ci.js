4658dcfdeb4532e38ed28ff994e427081110a9c2
Alfonso M. García Astorga <me@alfon.io>
2019-03-31 00:18:13 +0100

    update

--- packages/api/src/config/ci.js
+++ packages/api/src/config/ci.js
@@ -1,3 +1,4 @@
 module.exports = {
   MONGO_URL: 'mongodb://mongodb:27017/api',
+  JWT_SECRET: 'secret',
 };
--- packages/api/src/config/dev.js
+++ packages/api/src/config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGO_URL: 'mongodb://localhost:27017/api-saas-boilerplate',
-  JWT_SECRET: 'foo',
+  JWT_SECRET: 'secret',
   API_SECRET_KEY: 'foo',
   PRODUCT_APP_URL: 'http://localhost:3000',
 };
