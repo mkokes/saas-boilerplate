743125d6b338297a4bf986a00ace130cb59efb8f
Alfonso M. García Astorga <me@alfon.io>
2019-03-30 21:50:25 +0100

    update

---
+++ packages/api/scripts/fixtures/index.js
@@ -0,0 +1,11 @@
+const Fixtures = require('node-mongodb-fixtures');
+const fixtures = new Fixtures({
+  dir: 'scripts/fixtures/data',
+  mute: true,
+});
+
+fixtures
+  .connect('mongodb://localhost:27017/api-saas-boilerplate')
+  .then(() => fixtures.unload())
+  .then(() => fixtures.load())
+  .then(() => fixtures.disconnect());
