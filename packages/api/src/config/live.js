77ff09455560f804e729073aad2ad76c5ed0be5d
Alfonso M. García Astorga <me@alfon.io>
2019-04-11 20:01:18 +0200

    update

--- packages/api/src/config/live.js
+++ packages/api/src/config/live.js
@@ -1,4 +1,4 @@
 module.exports = {
   SENTRY_DSN: 'https://614c2c61a38141c584a4cc4e19a96f46@sentry.io/1385946',
-  PRODUCT_APP_URL: 'https://app-amgaventures.netlify.com',
+  PRODUCT_APP_URL: 'https://app.saas.amgaventures.com',
 };
