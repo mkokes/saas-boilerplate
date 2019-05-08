d75c98181649641608455c22887920650dfdbad9
Alfonso M. García Astorga <me@alfon.io>
2019-05-08 04:22:54 +0200

    Return observer.error on networkError and refactor code

--- packages/app/src/graphql/links/error.js
+++ packages/app/src/graphql/links/error.js
@@ -13,78 +13,82 @@ function onTokenRefreshed(err) {
   tokenSubscribers.map(cb => cb(err));
 }
 
-/* eslint-disable no-console, consistent-return */
+/* eslint-disable consistent-return, no-console */
 const errorLink = () =>
-  onError(({ graphQLErrors, networkError, operation, forward }) => {
-    if (networkError) console.error(`[Network error]: ${networkError}`);
+  onError(
+    ({ graphQLErrors, networkError, operation, forward }) =>
+      new Observable(async observer => {
+        if (networkError) {
+          console.error(`[Network error]: ${networkError}`);
+          return observer.error(networkError);
+        }
 
-    if (graphQLErrors) {
-      const { message, locations, path, extensions } = graphQLErrors[0];
-      console.error(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}, Code: ${
-          extensions.code
-        }`,
-      );
+        if (graphQLErrors) {
+          const { message, locations, path, extensions } = graphQLErrors[0];
+          console.error(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}, Code: ${
+              extensions.code
+            }`,
+          );
 
-      if (extensions.code === 'UNAUTHENTICATED') {
-        return new Observable(async observer => {
-          try {
-            const retryRequest = () => {
-              operation.setContext({
-                headers: {
-                  ...headers,
-                  Authorization: buildAuthHeader(
-                    globalProvider.authAccessToken(),
-                  ).Authorization,
-                },
-              });
+          if (extensions.code === 'UNAUTHENTICATED') {
+            try {
+              const retryRequest = () => {
+                operation.setContext({
+                  headers: {
+                    ...headers,
+                    Authorization: buildAuthHeader(
+                      globalProvider.authAccessToken(),
+                    ).Authorization,
+                  },
+                });
 
-              const subscriber = {
-                next: observer.next.bind(observer),
-                error: observer.error.bind(observer),
-                complete: observer.complete.bind(observer),
+                const subscriber = {
+                  next: observer.next.bind(observer),
+                  error: observer.error.bind(observer),
+                  complete: observer.complete.bind(observer),
+                };
+
+                return forward(operation).subscribe(subscriber);
               };
 
-              return forward(operation).subscribe(subscriber);
-            };
+              const { headers } = operation.getContext();
+              const globalProvider = await getGlobalProvider();
 
-            const { headers } = operation.getContext();
-            const globalProvider = await getGlobalProvider();
+              if (!isFetchingToken) {
+                isFetchingToken = true;
 
-            if (!isFetchingToken) {
-              isFetchingToken = true;
+                try {
+                  await globalProvider.refreshAccessTokenReq();
 
-              try {
-                await globalProvider.refreshAccessTokenReq();
+                  isFetchingToken = false;
+                  onTokenRefreshed(null);
+                  tokenSubscribers = [];
 
-                isFetchingToken = false;
-                onTokenRefreshed(null);
-                tokenSubscribers = [];
+                  return retryRequest();
+                } catch (e) {
+                  onTokenRefreshed(new Error('Unable to refresh access token'));
 
-                return retryRequest();
-              } catch (e) {
-                onTokenRefreshed(new Error('Unable to refresh access token'));
+                  tokenSubscribers = [];
+                  isFetchingToken = false;
 
-                tokenSubscribers = [];
-                isFetchingToken = false;
-
-                return globalProvider.logOut(true);
+                  return globalProvider.logOut(true);
+                }
               }
-            }
 
-            const tokenSubscriber = new Promise(resolve => {
-              subscribeTokenRefresh(errRefreshing => {
-                if (!errRefreshing) return resolve(retryRequest());
+              const tokenSubscriber = new Promise(resolve => {
+                subscribeTokenRefresh(errRefreshing => {
+                  if (!errRefreshing) return resolve(retryRequest());
+                });
               });
-            });
 
-            return tokenSubscriber;
-          } catch (e) {
-            observer.error(e);
+              return tokenSubscriber;
+            } catch (e) {
+              observer.error(e);
+            }
           }
-        });
-      }
-    }
-  });
+        }
+      }),
+  );
 
 export default errorLink;
