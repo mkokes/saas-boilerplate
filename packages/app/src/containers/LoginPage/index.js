2d67ee0b2543ff181746ef0de0132ddc8fa2395d
Alfonso M. García Astorga <me@alfon.io>
2019-05-13 06:02:34 +0200

    Refactor login page

--- packages/app/src/containers/LoginPage/index.js
+++ packages/app/src/containers/LoginPage/index.js
@@ -4,7 +4,7 @@
  *
  */
 
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import {
@@ -20,7 +20,7 @@ import {
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { Form, Field, withFormik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import { ReactstrapInput } from 'utils/formiik';
 import styled from 'styled-components';
@@ -58,7 +58,7 @@ export default class LoginPage extends React.PureComponent {
                         {({ setAuthTokens, logIn }) => (
                           <SafeMutation mutation={LOGIN_USER} showError>
                             {logInUserRequest => (
-                              <LoginFormWithFormik
+                              <LoginForm
                                 setAuthTokens={setAuthTokens}
                                 logIn={logIn}
                                 logInUserRequest={logInUserRequest}
@@ -83,117 +83,117 @@ export default class LoginPage extends React.PureComponent {
 }
 
 const LoginForm = props => {
-  const { isSubmitting, status } = props;
+  const { setAuthTokens, logIn, logInUserRequest } = props;
+
+  const [show2FALostMsg, setShow2FALostMsg] = useState(false);
+
   return (
-    <Form>
-      <Field
-        component={ReactstrapInput}
-        name="email"
-        type="email"
-        label="Email address"
-        autoComplete="email"
-        required
-      />
-      <Field
-        component={ReactstrapInput}
-        name="password"
-        type="password"
-        label="Password"
-        autoComplete="current-password"
-        required
-      />
-      <ForgotPasswordContainer className="text-right">
-        <Link to="/auth/forgot-password" className="text-muted">
-          forgot password?
-        </Link>
-      </ForgotPasswordContainer>
-      <Field
-        component={ReactstrapInput}
-        name="token"
-        type="text"
-        label="2FA authentication (if enabled)"
-        placeholder="Google Authenticator token"
-        autoComplete="off"
-      />
-      <Alert hidden={!status.show2FALostMsg} color="warning" fade={false}>
-        If you have lost your 2FA token, please open a{' '}
-        <Link to="/contact-support?subject=I%20lost%20my%202FA%20device&ticketType=LOST_2FA">
-          support ticket
-        </Link>
-        .
-      </Alert>
-      <Button
-        type="submit"
-        block
-        size="lg"
-        className="btn-theme"
-        disabled={isSubmitting}
-      >
-        <FontAwesomeIcon
-          pulse
-          icon={faSpinner}
-          className={isSubmitting ? 'mr-2' : 'd-none'}
-        />
-        Log In
-      </Button>
-    </Form>
+    <Formik
+      initialValues={{ email: '', password: '', token: '' }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .email('Invalid email')
+          .required('Required'),
+        password: Yup.string().required('Required'),
+        token: Yup.string(),
+      })}
+      onSubmit={async (values, formikBag) => {
+        setShow2FALostMsg(false);
+
+        try {
+          const { data } = await logInUserRequest({
+            variables: {
+              ...values,
+            },
+          });
+
+          const { accessToken, refreshToken } = data.loginUser;
+
+          await setAuthTokens({
+            accessToken,
+            refreshToken,
+          });
+
+          await logIn();
+          toast.info(`Logged in successfully.`, {
+            position: toast.POSITION.BOTTOM_LEFT,
+            hideProgressBar: true,
+            pauseOnHover: false,
+            autoClose: 3000,
+            draggable: false,
+          });
+        } catch (e) {
+          if (e.name === 'apollo_link_error' && e.type === 'BAD_USER_INPUT') {
+            formikBag.setErrors(e.data);
+
+            if ('token' in e.data) {
+              setShow2FALostMsg(true);
+            }
+          }
+
+          formikBag.setSubmitting(false);
+        }
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field
+            component={ReactstrapInput}
+            name="email"
+            type="email"
+            label="Email address"
+            autoComplete="email"
+            required
+          />
+          <Field
+            component={ReactstrapInput}
+            name="password"
+            type="password"
+            label="Password"
+            autoComplete="current-password"
+            required
+          />
+          <ForgotPasswordContainer className="text-right">
+            <Link to="/auth/forgot-password" className="text-muted">
+              forgot password?
+            </Link>
+          </ForgotPasswordContainer>
+          <Field
+            component={ReactstrapInput}
+            name="token"
+            type="text"
+            label="2FA authentication (if enabled)"
+            placeholder="Google Authenticator token"
+            autoComplete="off"
+          />
+          <Alert hidden={!show2FALostMsg} color="warning" fade={false}>
+            If you have lost your 2FA token, please open a{' '}
+            <Link to="/contact-support?subject=I%20lost%20my%202FA%20device&ticketType=LOST_2FA">
+              support ticket
+            </Link>
+            .
+          </Alert>
+          <Button
+            type="submit"
+            block
+            size="lg"
+            className="btn-theme"
+            disabled={isSubmitting}
+          >
+            <FontAwesomeIcon
+              pulse
+              icon={faSpinner}
+              className={isSubmitting ? 'mr-2' : 'd-none'}
+            />
+            Log In
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 LoginForm.propTypes = {
-  isSubmitting: PropTypes.bool,
-  status: PropTypes.object,
+  setAuthTokens: PropTypes.func,
+  logIn: PropTypes.func,
+  logInUserRequest: PropTypes.func,
 };
-
-const LoginFormWithFormik = withFormik({
-  mapPropsToStatus: () => ({ show2FALostMsg: false }),
-  mapPropsToValues: () => ({ email: '', password: '', token: '' }),
-  validationSchema: Yup.object().shape({
-    email: Yup.string()
-      .email('Invalid email')
-      .required('Required'),
-    password: Yup.string().required('Required'),
-    token: Yup.string(),
-  }),
-  handleSubmit: async (
-    values,
-    { props, setSubmitting, setStatus, setErrors },
-  ) => {
-    const { logIn, logInUserRequest, setAuthTokens } = props;
-
-    setStatus({ show2FALostMsg: false });
-    try {
-      const { data } = await logInUserRequest({
-        variables: {
-          ...values,
-        },
-      });
-
-      const { accessToken, refreshToken } = data.loginUser;
-
-      await setAuthTokens({
-        accessToken,
-        refreshToken,
-      });
-
-      await logIn();
-      toast.info(`Logged in successfully.`, {
-        position: toast.POSITION.BOTTOM_LEFT,
-        hideProgressBar: true,
-        pauseOnHover: false,
-        autoClose: 3000,
-        draggable: false,
-      });
-    } catch (e) {
-      if (e.name === 'apollo_link_error' && e.type === 'BAD_USER_INPUT') {
-        setErrors(e.data);
-
-        if ('token' in e.data) {
-          setStatus({ show2FALostMsg: true });
-        }
-      }
-
-      setSubmitting(false);
-    }
-  },
-  displayName: 'LoginForm',
-})(LoginForm);
