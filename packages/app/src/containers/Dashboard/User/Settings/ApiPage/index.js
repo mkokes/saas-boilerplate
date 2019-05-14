2be2a81cbf2f3c0d8ffd3c2265c021c8e67cfe21
Alfonso M. García Astorga <me@alfon.io>
2019-05-14 21:05:39 +0200

    Refactor api page

--- packages/app/src/containers/Dashboard/User/Settings/ApiPage/index.js
+++ packages/app/src/containers/Dashboard/User/Settings/ApiPage/index.js
@@ -5,21 +5,21 @@
  */
 
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
 import { Row, Col, Card, Button, Alert } from 'reactstrap';
 import { Formik, Form, Field } from 'formik';
-import { ApolloConsumer } from 'react-apollo';
 import { toast } from 'react-toastify';
 import { faQuestionCircle, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { transformApolloErr } from 'utils/apollo';
 import { REGENERATE_USER_API_SECRET_KEY } from 'graphql/mutations';
 import { ReactstrapInput } from 'utils/formiik';
 import SafeQuery from 'components/graphql/SafeQuery';
 import { USER_API_SECRET_KEY_QUERY } from 'graphql/queries';
 import config from 'config';
+import SafeMutation from 'components/graphql/SafeMutation';
 
 const { WEBSITE_URL } = config;
 
@@ -31,132 +31,129 @@ export default class ApiPage extends React.PureComponent {
         <Helmet>
           <title>Account API</title>
         </Helmet>
-        <ApolloConsumer>
-          {client => (
-            <Fragment>
-              <h1 className="mb-3">API</h1>
-              <Card body>
-                <legend>
-                  Credentials{' '}
-                  <a
-                    href={`${WEBSITE_URL}/api/information`}
-                    className="float-right"
-                    target="_new"
-                  >
-                    <small>
-                      API information{' '}
-                      <FontAwesomeIcon
-                        icon={faQuestionCircle}
-                        size="xs"
-                        className="align-middle"
-                      />
-                    </small>
-                  </a>
-                </legend>
-                <Row>
-                  <Col xs="12" sm="4">
-                    <p
-                      style={{
-                        fontWeight: 600,
-                        fontSize: 14,
-                      }}
-                      className="mb-0"
-                    >
-                      API Secret Key
-                    </p>
-                    <p style={{ fontWeight: 200, fontSize: 14 }}>
-                      Necessary when you want to make any API call on behalf of
-                      your account
-                    </p>
-                  </Col>
-                  <Col sm="7">
-                    <SafeQuery
-                      query={USER_API_SECRET_KEY_QUERY}
-                      keepExistingResultDuringRefetch
-                      fetchPolicy="network-only"
-                      showLoading
-                      showError
-                    >
-                      {({ data: { profile } }) => (
-                        <Formik
-                          initialValues={{
-                            apiSecretKey: profile.apiSecretKey,
-                          }}
-                          /* eslint-disable-next-line consistent-return */
-                          onSubmit={async (_, formikBag) => {
-                            try {
-                              const {
-                                data: { profile: updatedProfile },
-                              } = await client.mutate({
-                                mutation: REGENERATE_USER_API_SECRET_KEY,
-                              });
-
-                              formikBag.setFieldValue(
-                                'apiSecretKey',
-                                updatedProfile.apiSecretKey,
-                              );
-                              formikBag.setSubmitting(false);
-                            } catch (e) {
-                              const err = transformApolloErr(e);
-
-                              if (err.type === 'BAD_USER_INPUT') {
-                                formikBag.setErrors(err.data);
-                              } else {
-                                toast.error(err.message, {
-                                  position: toast.POSITION.TOP_CENTER,
-                                });
-                              }
-
-                              formikBag.setSubmitting(false);
-                            }
-                          }}
-                        >
-                          {({ values, isSubmitting }) => (
-                            <Fragment>
-                              <Form>
-                                <Alert color="primary" fade={false}>
-                                  Regenerating your secret key will invalidate
-                                  the existing token, and create a new one. You
-                                  should do this if you believe your token has
-                                  been compromised and wish to revoke API
-                                  access.
-                                </Alert>
-                                <Field
-                                  component={ReactstrapInput}
-                                  name="apiSecretKey"
-                                  label="API Secret Key"
-                                  value={values.apiSecretKey}
-                                  readOnly
-                                  style={{ display: 'inline-block' }}
-                                />
-
-                                <Button
-                                  type="submit"
-                                  disabled={isSubmitting}
-                                  className="float-right"
-                                >
-                                  <FontAwesomeIcon
-                                    pulse
-                                    icon={faSpinner}
-                                    className={isSubmitting ? 'mr-2' : 'd-none'}
-                                  />
-                                  Regenerate
-                                </Button>
-                              </Form>
-                            </Fragment>
-                          )}
-                        </Formik>
+        <Fragment>
+          <h1 className="mb-3">API</h1>
+          <Card body>
+            <legend>
+              Credentials{' '}
+              <a
+                href={`${WEBSITE_URL}/api/information`}
+                className="float-right"
+                target="_new"
+              >
+                <small>
+                  API information{' '}
+                  <FontAwesomeIcon
+                    icon={faQuestionCircle}
+                    size="xs"
+                    className="align-middle"
+                  />
+                </small>
+              </a>
+            </legend>
+            <Row>
+              <Col xs="12" sm="4">
+                <p
+                  style={{
+                    fontWeight: 600,
+                    fontSize: 14,
+                  }}
+                  className="mb-0"
+                >
+                  API Secret Key
+                </p>
+                <p style={{ fontWeight: 200, fontSize: 14 }}>
+                  Necessary when you want to make any API call on behalf of your
+                  account
+                </p>
+              </Col>
+              <Col sm="7">
+                <Alert color="primary" fade={false}>
+                  Regenerating your secret key will invalidate the existing
+                  token, and create a new one. You should do this if you believe
+                  your token has been compromised and wish to revoke API access.
+                </Alert>
+                <SafeQuery
+                  query={USER_API_SECRET_KEY_QUERY}
+                  keepExistingResultDuringRefetch
+                  fetchPolicy="network-only"
+                  showLoading
+                  showError
+                >
+                  {({ data: { profile: userProfile } }) => (
+                    <SafeMutation mutation={REGENERATE_USER_API_SECRET_KEY}>
+                      {regenerateApiSecretKeyRequest => (
+                        <RegenerateApiForm
+                          userProfile={userProfile}
+                          regenerateApiSecretKeyRequest={
+                            regenerateApiSecretKeyRequest
+                          }
+                        />
                       )}
-                    </SafeQuery>
-                  </Col>
-                </Row>
-              </Card>
-            </Fragment>
-          )}
-        </ApolloConsumer>
+                    </SafeMutation>
+                  )}
+                </SafeQuery>
+              </Col>
+            </Row>
+          </Card>
+        </Fragment>
       </Fragment>
     );
   }
 }
 
-ApiPage.propTypes = {};
+const RegenerateApiForm = props => {
+  const { userProfile, regenerateApiSecretKeyRequest } = props;
+
+  return (
+    <Formik
+      initialValues={{
+        apiSecretKey: userProfile.apiSecretKey,
+      }}
+      /* eslint-disable-next-line consistent-return */
+      onSubmit={async (_, formikBag) => {
+        try {
+          const {
+            data: {
+              profile: { apiSecretKey },
+            },
+          } = await regenerateApiSecretKeyRequest();
+
+          formikBag.setFieldValue('apiSecretKey', apiSecretKey);
+        } catch (e) {
+          toast.error(e.message, {
+            position: toast.POSITION.TOP_CENTER,
+          });
+        }
+
+        formikBag.setSubmitting(false);
+      }}
+    >
+      {({ values, isSubmitting }) => (
+        <Form>
+          <Field
+            component={ReactstrapInput}
+            name="apiSecretKey"
+            label="API Secret Key"
+            value={values.apiSecretKey}
+            readOnly
+            style={{ display: 'inline-block' }}
+          />
+
+          <Button type="submit" disabled={isSubmitting} className="float-right">
+            <FontAwesomeIcon
+              pulse
+              icon={faSpinner}
+              className={isSubmitting ? 'mr-2' : 'd-none'}
+            />
+            Regenerate
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+RegenerateApiForm.propTypes = {
+  userProfile: PropTypes.object,
+  regenerateApiSecretKeyRequest: PropTypes.func,
+};
