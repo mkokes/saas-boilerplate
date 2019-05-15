18a74fc999f08e23d0296ffdc4018678c2f77799
Alfonso M. García Astorga <me@alfon.io>
2019-05-16 01:31:59 +0200

    Add Delayed component

---
+++ packages/app/src/components/Delayed.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Delayed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hidden: true };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ hidden: false });
+    }, this.props.wait);
+  }
+
+  render() {
+    return this.state.hidden ? '' : this.props.children;
+  }
+}
+
+Delayed.propTypes = {
+  children: PropTypes.node,
+  wait: PropTypes.number.isRequired,
+};
+
+export default Delayed;
