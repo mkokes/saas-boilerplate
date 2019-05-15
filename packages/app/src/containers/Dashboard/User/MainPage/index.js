a8f42a3fc57e2af751971b7efc1e246512c90ad9
Alfonso M. García Astorga <me@alfon.io>
2019-05-15 03:53:33 +0200

    Refactor main page

--- packages/app/src/containers/Dashboard/User/MainPage/index.js
+++ packages/app/src/containers/Dashboard/User/MainPage/index.js
@@ -18,6 +18,58 @@ import { GlobalConsumer } from 'GlobalState';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class MainPage extends React.PureComponent {
+  renderMainAlerts(userProfile) {
+    return (
+      <Fragment>
+        <Alert
+          color="primary"
+          fade={false}
+          className="text-center"
+          hidden={
+            !(
+              !_.isEmpty(userProfile._subscription) &&
+              userProfile._subscription._plan.name === 'TRIAL' &&
+              moment(Number(userProfile._subscription.servicePeriodEnd)).diff(
+                new Date(),
+                'days',
+              ) <= 3
+            )
+          }
+        >
+          <span role="img" aria-label="bell icon">
+            🔔
+          </span>
+          <strong>
+            You have only{' '}
+            <Moment
+              date={
+                userProfile._subscription &&
+                Number(userProfile._subscription.servicePeriodEnd)
+              }
+              diff={new Date()}
+              unit="days"
+            />{' '}
+            day(s) left in your trial!{' '}
+            <Link to="/dashboard/settings/billing">Upgrade your plan</Link>{' '}
+          </strong>
+        </Alert>
+        <Alert
+          color="warning"
+          fade={false}
+          className="text-center"
+          hidden={!_.isEmpty(userProfile._subscription)}
+        >
+          <FontAwesomeIcon icon={faCreditCard} className="mr-2" size="lg" />
+          <strong>
+            Please{' '}
+            <Link to="/dashboard/settings/billing">upgrade your account</Link>{' '}
+            to continue using PRODUCT_NAME
+          </strong>
+        </Alert>
+      </Fragment>
+    );
+  }
+
   render() {
     return (
       <Fragment>
@@ -27,58 +79,7 @@ export default class MainPage extends React.PureComponent {
         <GlobalConsumer>
           {({ userProfile }) => (
             <Container tag="main">
-              <Alert
-                color="primary"
-                fade={false}
-                className="text-center"
-                hidden={
-                  !(
-                    !_.isEmpty(userProfile._subscription) &&
-                    userProfile._subscription._plan.name === 'TRIAL' &&
-                    moment(
-                      Number(userProfile._subscription.servicePeriodEnd),
-                    ).diff(new Date(), 'days') <= 3
-                  )
-                }
-              >
-                <span role="img" aria-label="bell icon">
-                  🔔
-                </span>
-                <strong>
-                  You have only{' '}
-                  <Moment
-                    date={
-                      userProfile._subscription &&
-                      Number(userProfile._subscription.servicePeriodEnd)
-                    }
-                    diff={new Date()}
-                    unit="days"
-                  />{' '}
-                  day(s) left in your trial!{' '}
-                  <Link to="/dashboard/settings/billing">
-                    Upgrade your plan
-                  </Link>{' '}
-                </strong>
-              </Alert>
-              <Alert
-                color="warning"
-                fade={false}
-                className="text-center"
-                hidden={!_.isEmpty(userProfile._subscription)}
-              >
-                <FontAwesomeIcon
-                  icon={faCreditCard}
-                  className="mr-2"
-                  size="lg"
-                />
-                <strong>
-                  Please{' '}
-                  <Link to="/dashboard/settings/billing">
-                    upgrade your account
-                  </Link>{' '}
-                  to continue using PRODUCT_NAME
-                </strong>
-              </Alert>
+              {this.renderMainAlerts(userProfile)}
               <h1>Main</h1>
               <Card body />
             </Container>
