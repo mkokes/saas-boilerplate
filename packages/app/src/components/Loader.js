ee65e74fd86f7a9d8716646e1480e759c067b793
Alfonso M. García Astorga <me@alfon.io>
2019-05-16 01:32:35 +0200

    Add render delay to Loader component

--- packages/app/src/components/Loader.js
+++ packages/app/src/components/Loader.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import Delayed from 'components/Delayed';
+
 const LoaderContainer = styled('div')`
   display: flex;
   justify-content: center;
@@ -12,37 +14,39 @@ const Circle = styled.circle`
 `;
 
 const Loader = ({ large }) => (
-  <LoaderContainer>
-    <svg
-      width={large ? '80' : '40'}
-      height={large ? '80' : '40'}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 100 100"
-      preserveAspectRatio="xMidYMid"
-      className="lds-rolling"
-    >
-      <Circle
-        cx="50"
-        cy="50"
-        fill="none"
-        strokeWidth="20"
-        r="35"
-        strokeDasharray="164.93361431346415 56.97787143782138"
-        transform="rotate(83.939 50 50)"
+  <Delayed wait={450}>
+    <LoaderContainer>
+      <svg
+        width={large ? '80' : '40'}
+        height={large ? '80' : '40'}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+        className="lds-rolling"
       >
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          calcMode="linear"
-          values="0 50 50;360 50 50"
-          keyTimes="0;1"
-          dur="1s"
-          begin="0s"
-          repeatCount="indefinite"
-        />
-      </Circle>
-    </svg>
-  </LoaderContainer>
+        <Circle
+          cx="50"
+          cy="50"
+          fill="none"
+          strokeWidth="20"
+          r="35"
+          strokeDasharray="164.93361431346415 56.97787143782138"
+          transform="rotate(83.939 50 50)"
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            calcMode="linear"
+            values="0 50 50;360 50 50"
+            keyTimes="0;1"
+            dur="1s"
+            begin="0s"
+            repeatCount="indefinite"
+          />
+        </Circle>
+      </svg>
+    </LoaderContainer>
+  </Delayed>
 );
 
 Loader.propTypes = {
