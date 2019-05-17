f611b11fdb41a1f8c6b92ceb7629b62cc4b11358
Alfonso M. García Astorga <me@alfon.io>
2019-05-17 23:36:13 +0200

    Format code FeedbackPage/loadable.js

--- packages/app/src/pages/FeedbackPage/Loadable.js
+++ packages/app/src/pages/FeedbackPage/Loadable.js
@@ -1,13 +1,11 @@
 /**
-*
-* Asynchronously loads the component for FeedbackPage
-*
-*/
+ * Asynchronously loads the component for FeedbackPage
+ */
 
 import loadable from 'loadable-components';
 
 import Loader from 'components/Loader';
 
 export default loadable(() => import('./index'), {
-LoadingComponent: Loader,
+  LoadingComponent: Loader,
 });
